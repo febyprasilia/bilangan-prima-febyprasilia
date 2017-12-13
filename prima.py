f223596b1e9210cdd7f80320054a339f48839e93
febyprasilia <febyprasiliaaragani.17@fasilkom.narotama.ac.id>
2017-12-13 10:25:40 +0700

    bilangan prima

---
+++ prima.py
@@ -0,0 +1,20 @@
+x=int(input("masukkan angka awal: "))
+y=int(input("masukkan batas: "))
+
+def bil1(x):
+    for a in range (b%c)==0:
+	    if range (a%x):
+	        a=True
+	    else:
+	 	    a=False
+	    print(a)
+
+for i in range(x,y):
+    prima=True
+    for j in range(2,i):
+        if(i%j==0):
+            prima=False
+    if(prima):
+        print(i,"adalah bilangan prima")
+    else:
+    	print(i,"adalah bukan bilangan prima")
