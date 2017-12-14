6049dea80ba4a45ee28287b71b613d2b9e5b37eb
febyprasilia <febyprasiliaaragani.17@fasilkom.narotama.ac.id>
2017-12-14 21:20:30 +0700

    bil prima

--- prima.py
+++ prima.py
@@ -1,20 +1,18 @@
+print("tugas alpro")
 x=int(input("masukkan angka awal: "))
 y=int(input("masukkan batas: "))
 
 def bil1(x):
-    for a in range (b%c)==1:
-	    if (a%x):
+    for a in range (b%c):
+	    if (a%x)==1:
 	        a=True
 	    else:
 	 	    a=False
 	    print(a)
-
 for i in range(x,y+1):
     prima=True
     for j in range(2,i):
         if(i%j==0):
             prima=False
     if(prima):
-        print(i,"adalah bilangan prima")
-    else:
-    	print(i,"adalah bukan bilangan prima")
+        print(i)
